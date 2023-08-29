303f65d379e365eb41ec51ee8f9231f5c1ffb3b7
Grzegorz Kowalik <gkowalik@future-processing.com>
2023-08-29 13:14:54 +0200

    Added changes related to more complicated scenarios to the sample projects.

---
+++ gatling-charts-highcharts-bundle-3.9.5/results/firstsimulation-20230823072114230/js/all_sessions.js
@@ -0,0 +1,11 @@
+allUsersData = {
+    
+color: '#FFA900',
+name: 'Active Users',
+data: [
+  [1692775276000,15],[1692775277000,12]
+],
+tooltip: { yDecimals: 0, ySuffix: '', valueDecimals: 0 }
+    , zIndex: 20
+    , yAxis: 1
+};
--- gatling-charts-highcharts-bundle-3.9.5/user-files/simulations/computerdatabase/FirstSimulation.java
+++ gatling-charts-highcharts-bundle-3.9.5/user-files/simulations/computerdatabase/FirstSimulation.java
@@ -14,39 +14,43 @@ import static io.gatling.javaapi.jdbc.JdbcDsl.*;
 public class FirstSimulation extends Simulation {
 
   private HttpProtocolBuilder httpProtocol = http
-    .baseUrl("http://computer-database.gatling.io")
-    .inferHtmlResources(AllowList(), DenyList(".*\\.js", ".*\\.css", ".*\\.gif", ".*\\.jpeg", ".*\\.jpg", ".*\\.ico", ".*\\.woff", ".*\\.woff2", ".*\\.(t|o)tf", ".*\\.png", ".*\\.svg", ".*detectportal\\.firefox\\.com.*"))
-    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8")
-    .acceptEncodingHeader("gzip, deflate")
-    .acceptLanguageHeader("en-US,en;q=0.7,pl;q=0.3")
-    .upgradeInsecureRequestsHeader("1")
-    .userAgentHeader("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0");
-  
-  private Map<CharSequence, String> headers_1 = Map.ofEntries(
-    Map.entry("Cache-Control", "no-cache"),
-    Map.entry("DNT", "1"),
-    Map.entry("Pragma", "no-cache")
-  );
+      .baseUrl("http://computer-database.gatling.io")
+      .inferHtmlResources(AllowList(),
+          DenyList(".*\\.js", ".*\\.css", ".*\\.gif", ".*\\.jpeg", ".*\\.jpg", ".*\\.ico", ".*\\.woff", ".*\\.woff2",
+              ".*\\.(t|o)tf", ".*\\.png", ".*\\.svg", ".*detectportal\\.firefox\\.com.*"))
+      .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8")
+      .acceptEncodingHeader("gzip, deflate")
+      .acceptLanguageHeader("en-US,en;q=0.7,pl;q=0.3")
+      .upgradeInsecureRequestsHeader("1")
+      .userAgentHeader("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0");
 
-  private ScenarioBuilder scn = scenario("FirstSimulation")
-    .exec(
+  private Map<CharSequence, String> headers_1 = Map.ofEntries(
+      Map.entry("Cache-Control", "no-cache"),
+      Map.entry("DNT", "1"),
+      Map.entry("Pragma", "no-cache"));
+
+  ChainBuilder homeScreen = exec(
       http("Home screen")
-        .get("/")
-    )
-    .pause(32)
-    .exec(
+          .get("/"));
+
+  ChainBuilder openSpecificComputer = exec(
       http("Open specific computer")
-        .get("/computers/501")
-        .headers(headers_1)
-    )
-    .pause(26)
-    .exec(
+          .get("/computers/501")
+          .headers(headers_1));
+
+  ChainBuilder searchForComputer = exec(
       http("Search for computer")
-      .get("/computers?f=macbook")
-      )
+          .get("/computers?f=macbook"));
+
+  ScenarioBuilder scenario1 = scenario("First Scenario")
+      .exec(homeScreen, openSpecificComputer);
+
+  ScenarioBuilder scenario2 = scenario("Second Scenario")
+      .exec(homeScreen, openSpecificComputer, searchForComputer);
   {
-	  setUp(
-      scn.injectOpen(atOnceUsers(100)))
-    .protocols(httpProtocol);
+    setUp(
+        scenario1.injectOpen(atOnceUsers(10)),
+        scenario2.injectOpen(atOnceUsers(5)))
+        .protocols(httpProtocol);
   }
 }
